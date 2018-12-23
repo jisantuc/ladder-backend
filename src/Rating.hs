41ba8cbe36d98329a416fdc62c8eba470fefe7d7
James Santucci <james.santucci@gmail.com>
2018-12-23 13:38:47 -0500

    Set up tests for matchSeqLikelihood

--- src/Rating.hs
+++ src/Rating.hs
@@ -1,14 +1,19 @@
 module Rating ( eloUpdate
               , eloUpdateWithConstant
-              , perMatchWinProbability
+              , matchSeqLikelihood
               , matchSeqWinProbability
+              , perMatchWinProbability
+              , race
               , validateMatchup ) where
 
 import           Data.Matchup                     (Matchup)
 import           Debug.Trace
 import           Error                            (Message (..))
-import           Statistics.Distribution          (cumulative)
+import           Statistics.Distribution          (cumulative, probability)
 import           Statistics.Distribution.Binomial (binomial)
+
+race :: Int
+race = 6
 
 eloUpdateWithConstant :: Int -> Double -> Double -> (Int, Int)
 eloUpdateWithConstant k winnerRating loserRating =
@@ -24,12 +29,32 @@ eloUpdate :: Double -> Double -> (Int, Int)
 eloUpdate winnerRating loserRating =
   eloUpdateWithConstant 20 winnerRating loserRating
 
-matchSeqLikelihood :: Int -> Int -> Double -> Double -> Double
-matchSeqLikelihood gamesWon oppGamesWon rating oppRating = undefined
+matchSeqLikelihood :: Int -> Double -> Double -> Double
+matchSeqLikelihood oppGamesWon rating oppRating =
+  -- Problem: given that I won, what was the prior likelihood that you would have won
+  -- no more than oppGamesWon racks?
+  -- possibly just simulate this out for some probabilities and make a lookup
+  let
+    favoredWon = rating > oppRating
+    perMatchSuccessRate =
+      perMatchWinProbability rating oppRating
+    winnerPerMatchSuccess =
+      if (favoredWon) then
+        perMatchSuccessRate
+      else
+        1 - perMatchSuccessRate
+  in
+    undefined
 
+{-Calculate the likelihood that the higher-ranked of two elo ratings wins a rack
+-}
 perMatchWinProbability :: Double -> Double -> Double
-perMatchWinProbability opponentRank playerRank =
-  ( 1 / ( 1 + 10 ** ((opponentRank - playerRank) / 400)) )
+perMatchWinProbability rank1 rank2 =
+  let
+    highRank = rank1 `max` rank2
+    lowRank = rank1 `min` rank2
+  in
+    ( 1 / ( 1 + 10 ** ((highRank - lowRank) / 400)) )
 
 {- Calculate the likelihood that a player with a given likelihood of winning each match
 wins the series of n matches, with a required number of matches to win
@@ -38,14 +63,16 @@ matchSeqWinProbability :: Int -> Double -> Double -> Double
 matchSeqWinProbability nMatches nMatchesMustWin perMatchProbability =
   1 - (cumulative (binomial (nMatches + 1) perMatchProbability) nMatchesMustWin)
 
+{- Reject matchups if the favored player is more than 75% likely to win based on elo
+-}
 validateMatchup :: Double -> Double -> Either Message ()
 validateMatchup rating1 rating2 =
   let
-    tilt = (matchSeqWinProbability 10 6 $ perMatchWinProbability rating1 rating2)
+    tilt = (matchSeqWinProbability 11 6 $ perMatchWinProbability rating1 rating2)
   in
-    if (tilt < 0.25 || 1 - tilt < 0.25) then
+    if (tilt < 0.25) then
       Left $ UnbalancedMatch ("Lower-ranked player has only a " ++
-                              show ((tilt `min` 1 - tilt) * 100) ++
+                              show (round ((tilt `min` (1 - tilt)) * 100)) ++
                               "% chance to win")
     else
       Right ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,10 @@
 module Main (main) where
 
-import           Rating     (eloUpdateWithConstant, matchSeqWinProbability, validateMatchup)
+import           Rating     ( eloUpdateWithConstant
+                            , matchSeqWinProbability
+                            , matchSeqLikelihood
+                            , validateMatchup
+                            , race)
 import           Test.Hspec
 
 main :: IO ()
@@ -22,3 +26,14 @@ spec = do
       validateMatchup 1000 1000 `shouldBe` Right ()
       validateMatchup 1000 1030 `shouldBe` Right ()
       validateMatchup 1000 1200 `shouldNotBe` Right ()
+      validateMatchup 1200 1000 `shouldNotBe` Right ()
+  describe "Post-match likelihood calculation" $ do
+    it "should think even matches are even" $ do
+      matchSeqLikelihood (race - 1) 1000 1000 `shouldSatisfy` (\x -> abs (x - 0.5) < 0.000001)
+    it "should think mostly even matches are also pretty even" $ do
+      matchSeqLikelihood (race - 1) 1001 1000 `shouldSatisfy` (\x -> abs (x - 0.5) < 0.05)
+      matchSeqLikelihood (race - 1) 1000 1001 `shouldSatisfy` (\x -> abs (x - 0.5) < 0.05)
+      matchSeqLikelihood (race - 1) 1010 1000 `shouldSatisfy` (\x -> abs (x - 0.5) < 0.05)
+      matchSeqLikelihood (race - 1) 1000 1010 `shouldSatisfy` (\x -> abs (x - 0.5) < 0.05)
+    it "should think unbalanced matchups are unbalanced" $ do
+      matchSeqLikelihood (race - 1) 5000 1000 `shouldSatisfy` (\x -> x < matchSeqLikelihood (race - 3) 5000 1000)
