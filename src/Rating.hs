2e0b287b814c60d00e56b281d1ec73486b549cd2
James Santucci <james.santucci@gmail.com>
2018-12-22 19:27:08 -0500

    Make fair (mostly) matches from ratings

--- src/Rating.hs
+++ src/Rating.hs
@@ -1,4 +1,15 @@
-module Rating ( eloUpdate ) where
+module Rating ( eloUpdate
+              , perMatchWinProbability
+              , matchSeqWinProbability
+              , makeFairMatch ) where
+
+import           Data.Matchup                     (Matchup)
+import           Statistics.Distribution          (cumulative)
+import           Statistics.Distribution.Binomial (binomial)
+
+data FairMatch = FairMatch { lowerRankedMustWin            :: Int
+                           , higherRankedMustWin           :: Int
+                           , lowerRankedVictoryProbability :: Double } deriving (Eq, Show)
 
 eloUpdate :: Double -> Double -> (Int, Int)
 eloUpdate winnerRating loserRating =
@@ -13,3 +24,36 @@ eloUpdate winnerRating loserRating =
     newLoserRating = loserRating + k * (0 - probLoser)
   in
     (round newWinnerRating, round newLoserRating)
+
+matchSeqLikelihood :: Int -> Int -> Double -> Double -> Double
+matchSeqLikelihood gamesWon oppGamesWon rating oppRating = undefined
+
+perMatchWinProbability :: Double -> Double -> Double
+perMatchWinProbability opponentRank playerRank =
+  ( 1 / ( 1 + 10 ** ((opponentRank - playerRank) / 400)) )
+
+{- Calculate the likelihood that a player with a given likelihood of winning each match
+wins the series of n matches, with a required number of matches to win
+-}
+matchSeqWinProbability :: Int -> Double -> Double -> Double
+matchSeqWinProbability nMatches nMatchesMustWin perMatchProbability =
+  1 - (cumulative (binomial (nMatches + 1) perMatchProbability) nMatchesMustWin)
+
+makeFairMatch :: Double -> Double -> FairMatch
+makeFairMatch rating1 rating2 =
+  let
+    lowerRankedPerMatchProbability =
+      perMatchWinProbability (rating1 `max` rating2) (rating1 `min` rating2)
+    m = 5
+  -- problem: choose a number of matches n and a win condition out of those matches m
+  -- such that:
+  --
+  -- cumulative (binomial n m) ~= 50%
+  in
+    go m lowerRankedPerMatchProbability
+  where
+    go numberOfMatches perMatchSuccessRate
+      | abs(matchSeqWinProbability 10 numberOfMatches perMatchSuccessRate - 0.5) <= 0.05 =
+        FairMatch (round numberOfMatches) (round $ 10 - numberOfMatches) (matchSeqWinProbability 10 numberOfMatches perMatchSuccessRate)
+      | otherwise =
+        go (numberOfMatches - 1) perMatchSuccessRate
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE QuasiQuotes       #-}
 module Main (main) where
 
-import           Rating     (eloUpdate)
+import           Rating     (eloUpdate, matchSeqWinProbability)
 import           Test.Hspec
 
 main :: IO ()
@@ -15,3 +15,8 @@ spec = do
       eloUpdate 1000 1200 `shouldBe` (1023, 1177)
     it "matches what's on https://www.geeksforgeeks.org/elo-rating-algorithm/ for a non-upset" $ do
       eloUpdate 1200 1000 `shouldBe` (1207, 993)
+  describe "Match sequence likelihood calculations" $ do
+    it "should think players with equal odds are equally likely to win" $ do
+      round (matchSeqWinProbability 10000 5000 0.5 * 100) `shouldBe` 50
+    it "should think probability is lower with lower per match probabilities" $ do
+      matchSeqWinProbability 10000 5000 0.9 `shouldSatisfy` (\x -> x > matchSeqWinProbability 10000 5000 0.1)
