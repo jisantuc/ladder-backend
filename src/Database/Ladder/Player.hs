21e5cdd9641fafa4346409145bedd0af4c712177
James Santucci <james.santucci@gmail.com>
2018-12-25 15:28:24 -0500

    Add player db CRUD (#13)

---
+++ src/Database/Ladder/Player.hs
@@ -0,0 +1,53 @@
+module Database.Ladder.Player ( getPlayer
+                              , createPlayer
+                              , updatePlayer
+                              , deletePlayer
+                              , listPlayers ) where
+
+import           Data.Int           (Int64)
+import           Data.Ladder.Player
+import           Data.UUID          (UUID)
+import qualified Database.Ladder    as Database
+import qualified Database.PostgreSQL.Simple       as Postgres
+import Database.PostgreSQL.Simple.SqlQQ
+
+getPlayer :: Database.Handle -> UUID -> IO [Player]
+getPlayer handle playerID =
+  let
+    fetchQuery = [sql|SELECT id, email, first_name, last_name, accepting_matches
+                     FROM players
+                     WHERE id = ?;|]
+  in
+    Postgres.query (Database.conn handle) fetchQuery (Postgres.Only playerID)
+
+createPlayer :: Database.Handle -> Player -> IO [Player]
+createPlayer handle player =
+  let
+    insertQuery = [sql|INSERT INTO players (id, email, first_name, last_name, accepting_matches)
+                      VALUES (?, ?, ?, ?, ?)
+                      RETURNING id, email, first_name, last_name, accepting_matches;|]
+  in
+    Postgres.query (Database.conn handle) insertQuery player
+
+updatePlayer :: Database.Handle -> Player -> IO Int64
+updatePlayer handle player =
+  let
+    updateQuery = [sql|UPDATE players
+                      SET email = ?, first_name = ?, last_name = ?, accepting_matches = ?
+                      WHERE id = ?;|]
+  in
+    Postgres.execute (Database.conn handle) updateQuery (toUpdate player)
+
+deletePlayer :: Database.Handle -> Player -> IO Int64
+deletePlayer handle player =
+  let
+    deleteQuery = [sql|DELETE FROM players WHERE id = ?;|]
+  in
+    Postgres.execute (Database.conn handle) deleteQuery (Postgres.Only $ playerID player)
+
+listPlayers :: Database.Handle -> IO [Player]
+listPlayers handle =
+  let
+    listQuery = [sql|SELECT id, email, first_name, last_name, accepting_matches FROM players;|]
+  in
+    Postgres.query_ (Database.conn handle) listQuery
