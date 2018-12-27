904e847ba12def46cb8854c3770155559d0aafae
James Santucci <james.santucci@gmail.com>
2018-12-26 20:39:21 -0500

    Add validation and instances

--- src/Data/Ladder/Match.hs
+++ src/Data/Ladder/Match.hs
@@ -1,11 +1,20 @@
 module Data.Ladder.Match ( Match ) where
 
-import qualified Data.Ladder.Time as Time
-import           Data.UUID        (UUID)
+import qualified Data.Ladder.Time                   as Time
+import           Data.UUID                          (UUID)
+import qualified Database.PostgreSQL.Simple.FromRow as Postgres
+import qualified Database.PostgreSQL.Simple.ToRow   as Postgres
+import           GHC.Generics                       (Generic)
+
 
 data Match = Match { matchID     :: UUID
                    , matchup     :: UUID
                    , startTime   :: Time.SqlTime
                    , recorded    :: Time.SqlTime
                    , player1Wins :: Int
-                   , player2Wins :: Int }
+                   , player2Wins :: Int
+                   , validated   :: Bool
+                   , submittedBy :: UUID } deriving (Eq, Show, Generic)
+
+instance Postgres.ToRow Match
+instance Postgres.FromRow Match
