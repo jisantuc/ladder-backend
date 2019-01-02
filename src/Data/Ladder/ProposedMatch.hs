38c04dde8b6cd619a6fe4863556e46df4fd50c43
James Santucci <james.santucci@gmail.com>
2019-01-01 20:04:47 -0600

    stylish

--- src/Data/Ladder/ProposedMatch.hs
+++ src/Data/Ladder/ProposedMatch.hs
@@ -1,16 +1,16 @@
 module Data.Ladder.ProposedMatch ( ProposedMatch (..) ) where
 
-import Data.UUID (UUID)
-import qualified Data.Ladder.Time as Time
-import GHC.Generics (Generic)
+import qualified Data.Ladder.Time                   as Time
+import           Data.UUID                          (UUID)
 import qualified Database.PostgreSQL.Simple.FromRow as Postgres
 import qualified Database.PostgreSQL.Simple.ToRow   as Postgres
+import           GHC.Generics                       (Generic)
 
 data ProposedMatch = ProposedMatch { proposedMatchID :: UUID
-                                   , matchup :: UUID
-                                   , proposedBy :: UUID
-                                   , venue :: UUID
-                                   , matchTime :: Time.SqlTime } deriving (Eq, Show, Generic)
+                                   , matchup         :: UUID
+                                   , proposedBy      :: UUID
+                                   , venue           :: UUID
+                                   , matchTime       :: Time.SqlTime } deriving (Eq, Show, Generic)
 
 instance Postgres.ToRow ProposedMatch
 instance Postgres.FromRow ProposedMatch
