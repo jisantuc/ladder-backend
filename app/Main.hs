e499f0df90a90f44fdaecca2f25b4570b8d9d725
James Santucci <james.santucci@gmail.com>
2019-01-03 11:44:10 -0600

    Stylish

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Server
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Server
 
 main :: IO ()
 main = do
